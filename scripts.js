bec7cb4857861f1d87b4b5f6bc53f89aa1d99ab7
Robert Bunch <rob@digitalcrafts.com>
2016-12-08 10:54:05 -0500

    Set up board and add marksquare function

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// 1. Set up board
+// 2. User should be able to click a box and a mark shows up.
+// -- put an onlick in the first square
+// -- when user clicks, call function that puts an X in the box.
+// MILESTONE
+
+function markSquare(square){
+	console.log(square.id);
+}
